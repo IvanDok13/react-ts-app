a1ef7c4a9bef95a2b139d848054a87b8302cd5be
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-14 11:53:38 -0700

    feat: add error btn

---
+++ src/components/error-button.tsx
@@ -0,0 +1,35 @@
+import type { JSX } from 'react';
+import { Component } from 'react';
+
+type State = {
+  hasError: boolean;
+};
+
+export class ErrorButton extends Component<unknown, State> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  public render(): JSX.Element {
+    const { hasError } = this.state;
+
+    if (hasError) {
+      throw new Error('The button throw an error');
+    }
+
+    return (
+      <button
+        className="errorButton button"
+        type="button"
+        onClick={() => {
+          this.setState({ hasError: true });
+        }}
+      >
+        Error button
+      </button>
+    );
+  }
+}
