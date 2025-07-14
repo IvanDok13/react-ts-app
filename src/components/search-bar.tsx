1ba5e81e093cdac25328702dda5948e3a34a2d82
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-14 11:52:19 -0700

    feat: add search bar

---
+++ src/components/search-bar.tsx
@@ -0,0 +1,33 @@
+import type { ChangeEvent, ReactNode } from 'react';
+import { Component } from 'react';
+
+interface Props {
+  onSearch: (term: string) => void;
+}
+interface State {
+  term: string;
+}
+
+export class SearchBar extends Component<Props, State> {
+  public state = { term: '' };
+
+  public handleChange = (event_: ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ term: event_.currentTarget.value });
+  };
+
+  public handleClick = (): void => {
+    const trimmed = this.state.term.trim();
+    this.props.onSearch(trimmed);
+  };
+
+  public render(): ReactNode {
+    return (
+      <div>
+        <input value={this.state.term} onChange={this.handleChange} placeholder="Pokemon name" />
+        <button className="button" onClick={this.handleClick}>
+          Go!
+        </button>
+      </div>
+    );
+  }
+}
