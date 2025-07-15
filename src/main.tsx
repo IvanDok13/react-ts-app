d5bede4effa3136f4cbdf34ff6e2c00bf49d0f90
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-15 13:03:26 -0700

    fix: update element creation

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 
-document.body.innerHTML = '<div id="app"></div>';
+const container = document.createElement('div');
+container.id = 'app';
+document.body.appendChild(container);
 
 const root = document.querySelector('#app');
+
 if (!(root instanceof HTMLElement)) {
   throw new TypeError('App element not found or is not an HTMLElement');
 }
 
-createRoot(document.querySelector('#app')!).render(
+createRoot(root).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
     </ErrorBoundary>
-  </StrictMode>
+  </StrictMode>,
 );
