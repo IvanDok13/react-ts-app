1cd3744789a7faf1a0487fed960f3eb2ea9f2e08
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-15 12:26:43 -0700

    feat: remove toastyfy

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -2,12 +2,3 @@ declare module '*.css' {
   const exports: { [exportName: string]: string };
   export = exports;
 }
-
-declare module 'toastify-js' {
-  const Toastify: {
-    (options: any): {
-      showToast: () => void;
-    };
-  };
-  export default Toastify;
-}
