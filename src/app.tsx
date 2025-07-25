adf15b3541e4deba1bdfcaa9a236554fc2e27b20
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-24 21:45:57 -0700

    refactor: change if statement, move interfaces and const

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,12 @@
 import { ErrorButton } from '@components/error-button';
+import type { PokemonFull } from '@interfaces/interface';
 import type { ReactNode } from 'react';
 import { Component } from 'react';
-import type { PokemonFull } from './api';
 import { fetchPokemonFull, fetchPokemonList } from './api';
 import styles from './app.module.css';
 import { PokemonList } from './components/pokemon-list';
 import { SearchBar } from './components/search-bar';
+import { ITEM_PER_PAGE, LAST_TERM } from './const/const';
 
 interface State {
   items: PokemonFull[];
@@ -24,9 +25,8 @@ export class App extends Component<Record<string, never>, State> {
   }
 
   public componentDidMount(): void {
-    const lastTerm = localStorage.getItem('searchTerm');
-    if (lastTerm) {
-      this.handleSearch(lastTerm);
+    if (LAST_TERM) {
+      this.handleSearch(LAST_TERM);
     } else {
       this.handleSearch('');
     }
@@ -38,13 +38,10 @@ export class App extends Component<Record<string, never>, State> {
     this.setState({ loading: true, error: null, items: [] });
 
     fetchPokemonList(trimmed)
-      .then(list => Promise.all(list.slice(0, 20).map(p => fetchPokemonFull(p.url))))
+      .then(list => Promise.all(list.slice(0, ITEM_PER_PAGE).map(p => fetchPokemonFull(p.url))))
       .then(fulls => this.setState({ items: fulls, loading: false }))
       .catch((error: unknown) => {
-        let errorMessage = 'Unknown error';
-        if (error instanceof Error) {
-          errorMessage = error.message;
-        }
+        const errorMessage = error instanceof Error ? error.message : 'Unknown error';
         this.setState({ error: errorMessage, loading: false });
       });
   };
