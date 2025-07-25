84bb59ad0950726c8f50d6edd944bd4f95180ca9
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-24 21:38:21 -0700

    feat: add interfaces fix magic numbers

---
+++ src/const/const.ts
@@ -0,0 +1,4 @@
+export const ITEM_PER_PAGE = 20;
+export const LIMIT_POKEMON = 1300;
+export const LAST_TERM = localStorage.getItem('searchTerm');
+export const API = 'https://pokeapi.co/api/v2/pokemon';
