232026a149d357787f5671377e8172b77e821811
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-24 21:43:45 -0700

    feat: add try catch block, move interfaces and const

--- src/api.ts
+++ src/api.ts
@@ -1,34 +1,26 @@
+import { API, LIMIT_POKEMON } from '@const/const';
+import { Pokemon, PokemonFull, PokemonListResponse } from '@interfaces/interface';
 import axios from 'axios';
 
-export interface Pokemon {
-  name: string;
-  url: string;
-}
-
-export interface PokemonFull {
-  id: number;
-  name: string;
-  sprites: { front_default: string | null };
-  types: { type: { name: string } }[];
-}
-
-interface PokemonListResponse {
-  results: Pokemon[];
-}
-
 export const fetchPokemonList = async (query: string = ''): Promise<Pokemon[]> => {
-  const limit = 1300;
-  const offset = query ? 0 : Math.floor(Math.random() * 1000);
+  try {
+    const offset = query ? 0 : Math.floor(Math.random() * 1000);
+    const response = await axios.get<PokemonListResponse>(`${API}?limit=${LIMIT_POKEMON}&offset=${offset}`);
+    const { results } = response.data;
 
-  const response = await axios.get<PokemonListResponse>(
-    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`,
-  );
-  const { results } = response.data;
-
-  return query ? results.filter(p => p.name.toLowerCase().includes(query.toLowerCase())) : results;
+    return query ? results.filter(p => p.name.toLowerCase().includes(query.toLowerCase())) : results;
+  } catch (error) {
+    console.error('Error fetching Pokémon list:', error);
+    throw new Error('Network Error');
+  }
 };
 
 export const fetchPokemonFull = async (url: string): Promise<PokemonFull> => {
-  const response = await axios.get<PokemonFull>(url);
-  return response.data;
+  try {
+    const response = await axios.get<PokemonFull>(url);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching full Pokémon data:', error);
+    throw new Error('API Error');
+  }
 };
