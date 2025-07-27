462cb5652c3c0fad7d745c99daf36a48611b3673
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-27 09:57:56 -0700

    refactor: update const

--- src/consts/const.ts
+++ src/consts/const.ts
@@ -1,4 +1,4 @@
-export const ITEM_PER_PAGE = 20;
+export const ITEM_PER_PAGE = 10;
 export const LIMIT_POKEMON = 1300;
 export const LAST_TERM = localStorage.getItem('searchTerm');
 export const API = 'https://pokeapi.co/api/v2/pokemon';
