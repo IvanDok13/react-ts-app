71576ca94511adb67da1705e354c00a0621daeb0
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-27 09:49:50 -0700

    feat: update header with about btn

--- src/components/header-el/header.tsx
+++ src/components/header-el/header.tsx
@@ -1,4 +1,5 @@
 import { SearchBar } from '@components/search-bar';
+import { AppRoutes } from '@router/routes';
 import { Link } from 'react-router-dom';
 import styles from './header.module.css';
 
@@ -10,10 +11,13 @@ export function Header({ onSearch }: Props) {
   return (
     <header className={styles.header}>
       <div className={styles.headerWrapper}>
-        <Link to="/" className={styles.logoLink}>
-          <img className={styles.logo} src="/svg/logo.svg" alt="Logo" />
+        <Link to={AppRoutes.HOME_ROUTE} className={styles.logoLink}>
+          <img className={styles.logo} src="https://www.svgrepo.com/show/276264/pokeball-pokemon.svg" alt="Logo" />
         </Link>
         <h1 className={styles.title}>PokéFinder</h1>
+        <Link to={AppRoutes.ABOUT_ROUTE} className={styles.logoLink}>
+          About
+        </Link>
         <SearchBar onSearch={onSearch} />
       </div>
     </header>
