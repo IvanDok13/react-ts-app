0ab1d582c0e1bd59b1ee0a3bc95caa865128c2db
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-27 14:24:51 -0700

    fix: remove unnecessary attributes

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -8,23 +8,9 @@ export interface PokemonFull {
   name: string;
   sprites: { front_default: string | null };
   types: { type: { name: string } }[];
-  height: number;
-  weight: number;
-  abilities: { ability: { name: string } }[];
-  stats: { base_stat: number; stat: { name: string } }[];
-  base_experience: number;
-  species: { name: string; url: string };
-  moves: { move: { name: string } }[];
-  game_indices: { game_index: number; version: { name: string } }[];
-  location_area_encounters: string;
-  is_default: boolean;
-  order: number;
-  forms: { name: string; url: string }[];
-  version_details: {
-    rarity: number;
-    version: { name: string; url: string };
-    moves: { move: { name: string } }[];
-  }[];
+  description?: string;
+  experience?: number;
+  abilities?: { ability: { name: string } }[];
 }
 
 export interface PokemonListResponse {
