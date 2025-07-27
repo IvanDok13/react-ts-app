bf014bef6a063e640aa1b1c3ed5a3ebfc90ccb37
Ivan Klochkov <vany-nov13@yandex.ru>
2025-07-26 19:49:23 -0700

    feat: update main whith routs

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { ErrorBoundary } from '@components/error-boundary';
+import { AppRouter } from '@router/router';
 import '@styles/styles.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app';
 
 const container = document.createElement('div');
 container.id = 'app';
@@ -17,7 +17,7 @@ if (!(root instanceof HTMLElement)) {
 createRoot(root).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <AppRouter />
     </ErrorBoundary>
   </StrictMode>,
 );
