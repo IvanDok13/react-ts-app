63a28f0aaed5406b5ca3fc83ef7ccc8a8bc66b76
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-04 09:20:43 +0400

    feat: update pokemon-card whith checkboxes

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,4 +1,6 @@
+import { useAppDispatch, useAppSelector } from '@hooks/UseSelect';
 import { PokemonFull } from '@interfaces/interface';
+import { toggleSelected } from '@store/selectedPokemon';
 
 interface Props {
   pokemon: PokemonFull;
@@ -6,6 +8,14 @@ interface Props {
 }
 
 export function PokemonCard({ pokemon, onClick = () => {} }: Props) {
+  const dispatch = useAppDispatch();
+  const isSelected = useAppSelector(state => Boolean(state.selected.selected[pokemon.id]));
+
+  const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.stopPropagation();
+    dispatch(toggleSelected(pokemon));
+  };
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
     if (event.key === 'Enter' || event.key === ' ') {
       onClick();
@@ -13,14 +23,17 @@ export function PokemonCard({ pokemon, onClick = () => {} }: Props) {
   };
 
   return (
-    <button
-      type="button"
-      className="pokemon-card"
-      onClick={onClick}
-      onKeyDown={handleKeyDown}
-      style={{ cursor: 'pointer' }}
-    >
-      <h4>{pokemon.name}</h4>
+    <button type="button" className="pokemon-card" onClick={onClick} onKeyDown={handleKeyDown}>
+      <label className="pokemon-checkbox" htmlFor="pokemon-checkbox">
+        <input
+          id="pokemon-checkbox"
+          type="checkbox"
+          checked={isSelected}
+          onChange={handleCheck}
+          onClick={e => e.stopPropagation()}
+        />
+        <h4>{pokemon.name}</h4>
+      </label>
       {pokemon.sprites.front_default && <img src={pokemon.sprites.front_default} alt={pokemon.name} />}
       <p>Type: {pokemon.types?.map(t => t.type.name).join(', ') || 'Unknown'}</p>
     </button>
