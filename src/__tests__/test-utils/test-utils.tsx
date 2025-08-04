3ab9890f0a72d2b6aaf0a017a00e7a3c3eef93e9
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-04 11:49:09 +0400

    feat: add test utils

--- src/__tests__/test-utils/test-utils.tsx
+++ src/__tests__/test-utils/test-utils.tsx
@@ -1,5 +1,16 @@
+import { ThemeProvider } from '@context/themeContext';
 import { store } from '@store/store';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+export const renderWithProviders = (component: React.ReactElement) => {
+  return render(
+    <ThemeProvider>
+      <BrowserRouter>{component}</BrowserRouter>
+    </ThemeProvider>,
+  );
+};
 
 export const withProviders = (children: React.ReactElement) => {
   return <Provider store={store}>{children}</Provider>;
