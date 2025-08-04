e430dd0060016374043f1a1da334cec05820b2fd
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-04 10:40:16 +0400

    refactor: update paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { ErrorBoundary } from '@components/error-boundary';
+import { ThemeProvider } from '@context/themeContext';
 import { AppRouter } from '@router/router';
+import { store } from '@store/store';
 import '@styles/styles.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from './context/themeContext';
-import { store } from './store/store';
 
 const container = document.createElement('div');
 container.id = 'app';
