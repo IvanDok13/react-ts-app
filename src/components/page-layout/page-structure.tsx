f0bb063983ae7b48d72d0a78e7c92af1847e752b
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-09 12:27:26 +0400

    fix: update component with useStorage hook

--- src/components/page-layout/page-structure.tsx
+++ src/components/page-layout/page-structure.tsx
@@ -1,9 +1,9 @@
 import { fetchPokemonFull, fetchPokemonList } from '@apis/api';
 import { ErrorButton } from '@components/error-button';
-import { Footer } from '@components/footer-el/footer';
-import { Header } from '@components/header-el/header';
+import { Footer } from '@components/footer/footer';
+import { Header } from '@components/header/header';
 import { SelectedBanner } from '@components/selected-banner/selected-banner';
-import { LAST_TERM } from '@const/const';
+import { useStorage } from '@hooks/UseStorage';
 import type { PokemonFull } from '@interfaces/interface';
 import { useCallback, useEffect, useState } from 'react';
 import { Outlet, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
@@ -16,7 +16,8 @@ export function PageLayout() {
   const [items, setItems] = useState<PokemonFull[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [term, setTerm] = useState<string>(LAST_TERM ?? '');
+  const { getStorage } = useStorage();
+  const [term, setTerm] = useState<string>(getStorage ?? '');
 
   const handleSearch = useCallback(
     (newTerm: string): void => {
