ed043c3dbe613f5821bde89d560bd841bc64c140
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-09 12:26:26 +0400

    fix: remove localStorage from const

--- src/consts/const.ts
+++ src/consts/const.ts
@@ -1,6 +1,5 @@
 export const ITEM_PER_PAGE = 10;
 export const LIMIT_POKEMON = 1300;
-export const LAST_TERM = localStorage.getItem('searchTerm');
 export const API = 'https://pokeapi.co/api/v2/pokemon';
 export const PAGE_BACK = -1;
 export const THEME_KEY = 'theme';
