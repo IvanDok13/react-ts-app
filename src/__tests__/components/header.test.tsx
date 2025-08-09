2dc5b1e0feb70e9964c81b5d8b01f644972a5cfd
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-09 12:25:52 +0400

    feat: update themeProvider and move files

--- src/__tests__/components/header.test.tsx
+++ src/__tests__/components/header.test.tsx
@@ -1,11 +1,11 @@
-import { Header } from '@components/header-el/header';
+import { Header } from '@components/header/header';
 import { fireEvent, screen } from '@testing-library/react';
-import { renderWithProviders } from 'src/__tests__/test-utils/test-utils';
+import { renderThemeProvider } from 'src/__tests__/test-utils/test-utils';
 import { describe, expect, it, vi } from 'vitest';
 
 describe('Header', () => {
   it('renders logo, title, and About link', () => {
-    renderWithProviders(<Header onSearch={() => {}} />);
+    renderThemeProvider(<Header onSearch={() => {}} />);
 
     expect(screen.getByText(/pokéfinder/i)).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /about/i })).toHaveAttribute('href', '/about');
@@ -14,7 +14,7 @@ describe('Header', () => {
 
   it('calls onSearch when SearchBar is used', () => {
     const handleSearch = vi.fn();
-    renderWithProviders(<Header onSearch={handleSearch} />);
+    renderThemeProvider(<Header onSearch={handleSearch} />);
 
     const input = screen.getByPlaceholderText(/pokemon name/i);
     fireEvent.change(input, { target: { value: 'squirtle' } });
--- src/__tests__/components/page-structure.test.tsx
+++ src/__tests__/components/page-structure.test.tsx
@@ -1,11 +1,12 @@
 import * as api from '@apis/api';
 import { PageLayout } from '@components/page-layout/page-structure';
 import type { PokemonFull } from '@interfaces/interface';
-import { render, screen, waitFor } from '@testing-library/react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { screen, waitFor } from '@testing-library/react';
+import { Route, Routes } from 'react-router-dom';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { renderThemeProvider } from '../test-utils/test-utils';
 
-vi.mock('@components/header-el/header', () => ({
+vi.mock('@components/header/header', () => ({
   Header: ({ onSearch }: { onSearch: (term: string) => void }) => (
     <button type="button" data-testid="mock-header" onClick={() => onSearch('pikachu')}>
       Mock Header
@@ -13,7 +14,7 @@ vi.mock('@components/header-el/header', () => ({
   ),
 }));
 
-vi.mock('@components/footer-el/footer', () => ({
+vi.mock('@components/footer/footer', () => ({
   Footer: () => <div data-testid="mock-footer">Mock Footer</div>,
 }));
 
@@ -41,18 +42,16 @@ describe('PageLayout component', () => {
     vi.spyOn(api, 'fetchPokemonList').mockResolvedValueOnce([{ name: 'pikachu', url: '' }]);
     vi.spyOn(api, 'fetchPokemonFull').mockResolvedValueOnce(mockPokemon);
 
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Routes>
-          <Route path="/" element={<PageLayout />}>
-            <Route index element={<div>Mock Home</div>} />
-          </Route>
-        </Routes>
-      </MemoryRouter>,
+    renderThemeProvider(
+      <Routes>
+        <Route path="/" element={<PageLayout />}>
+          <Route index element={<div>Mock Home</div>} />
+        </Route>
+      </Routes>,
     );
 
     expect(await screen.findByTestId('mock-header')).toBeInTheDocument();
-    expect(screen.getByTestId('mock-footer')).toBeInTheDocument();
+    expect(await screen.findByTestId('mock-footer')).toBeInTheDocument();
     expect(screen.getByTestId('mock-error-button')).toBeInTheDocument();
     expect(await screen.findByText('Mock Home')).toBeInTheDocument();
     expect(screen.getByTestId('mock-selected-banner')).toBeInTheDocument();
@@ -61,14 +60,12 @@ describe('PageLayout component', () => {
   it('handles search error correctly', async () => {
     vi.spyOn(api, 'fetchPokemonList').mockRejectedValueOnce(new Error('Fetch failed'));
 
-    render(
-      <MemoryRouter initialEntries={['/']}>
-        <Routes>
-          <Route path="/" element={<PageLayout />}>
-            <Route index element={<div>Fallback</div>} />
-          </Route>
-        </Routes>
-      </MemoryRouter>,
+    renderThemeProvider(
+      <Routes>
+        <Route path="/" element={<PageLayout />}>
+          <Route index element={<div>Fallback</div>} />
+        </Route>
+      </Routes>,
     );
 
     await waitFor(() => {
