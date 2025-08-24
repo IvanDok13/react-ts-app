0d448f4e43ca8b18ac73508f0f261b6bdadc5f74
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-25 00:02:55 +0300

    refactor: render img via an Image component from next/image

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,7 @@
+import { LangSwitcher } from '@components/lang-switcher';
 import { SearchBar } from '@components/search-bar';
 import { ThemeSwitcher } from '@components/theme-switcher/theme-switcher';
+import Image from 'next/image';
 import Link from 'next/link';
 import styles from './header.module.css';
 
@@ -12,12 +14,13 @@ export function Header({ onSearch }: Props) {
     <header className={styles.header}>
       <div className={styles.headerWrapper}>
         <Link href="/" className={styles.logoLink}>
-          <img className={styles.logo} src="https://www.svgrepo.com/show/276264/pokeball-pokemon.svg" alt="Logo" />
+          <Image width={50} height={50} src="https://www.svgrepo.com/show/276264/pokeball-pokemon.svg" alt="Logo" />
         </Link>
         <h1 className={styles.title}>PokéFinder</h1>
         <Link href="/about" className={styles.logoLink}>
           About
         </Link>
+        <LangSwitcher />
         <SearchBar onSearch={onSearch} />
         <ThemeSwitcher />
       </div>
