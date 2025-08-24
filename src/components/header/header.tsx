e27e8d0992f5e242b8a150380584598fda410ff8
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-24 15:55:22 +0300

    fix: change classNames

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,6 @@
 import { SearchBar } from '@components/search-bar';
 import { ThemeSwitcher } from '@components/theme-switcher/theme-switcher';
-import { AppRoutes } from '@router/routes';
-import { Link } from 'react-router-dom';
+import Link from 'next/link';
 import styles from './header.module.css';
 
 interface Props {
@@ -12,11 +11,11 @@ export function Header({ onSearch }: Props) {
   return (
     <header className={styles.header}>
       <div className={styles.headerWrapper}>
-        <Link to={AppRoutes.HOME_ROUTE} className={styles.logoLink}>
+        <Link href="/" className={styles.logoLink}>
           <img className={styles.logo} src="https://www.svgrepo.com/show/276264/pokeball-pokemon.svg" alt="Logo" />
         </Link>
         <h1 className={styles.title}>PokéFinder</h1>
-        <Link to={AppRoutes.ABOUT_ROUTE} className={styles.logoLink}>
+        <Link href="/about" className={styles.logoLink}>
           About
         </Link>
         <SearchBar onSearch={onSearch} />
