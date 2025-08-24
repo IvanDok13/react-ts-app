1925fadba142eaa92570618b49730ffa29e21f74
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-25 00:03:17 +0300

    feat: update next config

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,11 @@ const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   distDir: './dist',
   images: {
-    remotePatterns: [new URL('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/**')],
+    remotePatterns: [
+      new URL('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/**'),
+      new URL('https://avatars.githubusercontent.com/u/113414074?v=4'),
+      new URL('https://avatars.githubusercontent.com/u/11501370?s=200&v=4'),
+    ],
   },
 };
 
